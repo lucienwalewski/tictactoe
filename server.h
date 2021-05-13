52f56c9b073b50f54b8b266d9dcbb9a5a5b30f9e
PUECH Alban <72336171+albanpuech@users.noreply.github.com>
2021-05-13 09:51:36 +0200

    Amazing change

--- server.h
+++ server.h
@@ -4,7 +4,29 @@
  * Lucien Walewski and Alban Puech
  */
 
+
+// structure to store the arguments to give to the function while using pthread_create 
+// https://stackoverflow.com/questions/16230542/passing-multiple-arguments-to-threaded-function-from-pthread-create
+struct thread_arg {                                                   
+  char buffer[MAX_LEN_MSG]; 
+  struct sockaddr from;                                                             
+}; 
+
 struct clientinfos {
-    char client1[8];
-    char client2[8];
+    char client1[8]="00000000";
+    char client2[8]="00000000";
+    
 };
+
+
+
+
+int grid[9];
+memset(&grid,0,sizeof(grid));
+
+int mysocket;
+pthread_mutex_t lock;
+struct clientinfos clientinfo;
+
+
+
